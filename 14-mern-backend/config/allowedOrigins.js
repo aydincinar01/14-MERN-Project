56070d2732bb4d8855c2e1e300cee24a5d104346
Aydin Cinar <aydincinar01@hotmail.com>
2022-11-24 19:10:55 +0000

    Add files via upload

---
+++ 14-mern-backend/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'http://localhost',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3500'
+    
+]
+
+module.exports = allowedOrigins
